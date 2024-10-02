9035fd78eff04adfe13960a9e2930cd061ceae4c
ravinasrivastava <90607740+ravinasrivastava@users.noreply.github.com>
2024-10-02 17:16:56 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ChakraProvider } from "@chakra-ui/react";
+ import { Workflow } from "./Workflow/Workflow";
+
+function App() {
+ 
+  return (
+    <ChakraProvider>
+      <Workflow />
+    </ChakraProvider>
+  );
+}
+
+export default App;
